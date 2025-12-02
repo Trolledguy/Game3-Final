55cee83ca43a1faeeceee5c99d9c2170badbaf22
MoriKung <perfect83129@gmail.com>
2025-12-03 06:08:12 +0700

    NewDungeons

---
+++ Assets/Script/AI/Dinosour.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+
+public class Dinosour : Enemy
+{
+    protected override void PatrolBehavior()
+    {
+        base.PatrolBehavior();
+        // Custom patrol behavior for Dinosour can be added here
+    }
+}
